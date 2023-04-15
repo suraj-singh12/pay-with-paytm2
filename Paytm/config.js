e50e64e1b9e1c00de6320eddb35280143fc8c963
Suraj Singh <surajsingh5092@gmail.com>
2023-04-15 17:02:21 +0530

    Update config.js

--- Paytm/config.js
+++ Paytm/config.js
@@ -1,8 +1,8 @@
 var PaytmConfig = {
-  mid: "YjyPSF45602447857016",    // Test Merchant ID (tmRoBh90182073607271)  BfXSEL99752579173271
-//   mid: "blvlTo90281587777835",
-  key: "_rlkmfFOIK4lGP6#",        // Test Merchant Key (fAPNNg3!h3K60ZsM)   5#_5GcuCpHsNSRRu
-//   key: "xlXVZrOyKFYYau80",
+//   mid: "YjyPSF45602447857016",    // Test Merchant ID (tmRoBh90182073607271)  BfXSEL99752579173271
+  mid: "BfXSEL99752579173271",
+//   key: "_rlkmfFOIK4lGP6#",        // Test Merchant Key (fAPNNg3!h3K60ZsM)   5#_5GcuCpHsNSRRu
+  key: "5#_5GcuCpHsNSRRu",
   website: "WEBSTAGING",          // website
 };
 module.exports.PaytmConfig = PaytmConfig;
