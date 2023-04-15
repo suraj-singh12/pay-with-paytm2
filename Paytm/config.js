037cc16040f8fff0a7ef989ae452c5fa15e8f7b8
Suraj Singh <surajsingh5092@gmail.com>
2023-04-15 10:42:06 +0530

    Update config.js

--- Paytm/config.js
+++ Paytm/config.js
@@ -1,5 +1,6 @@
 var PaytmConfig = {
-  mid: "YjyPSF45602447857016",    // Test Merchant ID (tmRoBh90182073607271)  BfXSEL99752579173271
+//   mid: "YjyPSF45602447857016",    // Test Merchant ID (tmRoBh90182073607271)  BfXSEL99752579173271
+  mid: "tmRoBh90182073607271",
   key: "_rlkmfFOIK4lGP6#",        // Test Merchant Key (fAPNNg3!h3K60ZsM)   5#_5GcuCpHsNSRRu
   website: "WEBSTAGING",          // website
 };
